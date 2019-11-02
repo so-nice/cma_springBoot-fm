f58387b7fc6a50ea22996d05b731dd3a0fededc3
so-nice <chenjianqi@cma.gov.cn>
2019-11-03 00:00:00 +0800

    cma_springBoot-fm

---
+++ src/test/java/com/cma/fm/FmApplicationTests.java
@@ -0,0 +1,22 @@
+package com.yuxin.fm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *    中国气象局
+ *    待上线域名cma.gov.cn
+ *    http://39.103.177.68/  admin:cma
+ *    chenjianqi@cma.gov.cn
+ * */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
